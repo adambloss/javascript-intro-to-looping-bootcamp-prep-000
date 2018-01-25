8b272cb4c1831041f43935b12405f07a4f531343
Adam Bloss <adam.bloss17@gmail.com>
2018-01-25 19:37:23 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,19 @@
+function forLoop(array) {
+  var i = 0
+  array = []
+for(; i<25; i++) {
+  if(i===1){
+    return array.push('I am 1 strange loop.')
+  }
+  else {
+    return array.push('I am '+i+' strange loops.')
+  }
+} return array
+}
+
+function whileLoop(n) {
+  console.log(n);
+while(number > 0){
+  console.log(--n)
+}
+}
